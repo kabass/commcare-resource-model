980c6db5e7f38b1ff7032c7fcca42aa7f2a77d14
Simon Kelly <skelly@dimagi.com>
2019-01-19 10:31:16 +0530

    don't cast to float

--- core/summarize.py
+++ core/summarize.py
@@ -119,8 +119,8 @@ def get_summary_data(config, service_data):
             data_storage_per_vm = data_storage_per_vm_lt + data_storage_per_vm_gt
         elif not compute['VMs'].any():
             # if this service doesn't have any compute resources
-            storage_buffer = data_storage * float(storage_estimation_buffer)
-            storage_estimation_buffer = data_storage * float(estimation_buffer)
+            storage_buffer = data_storage * storage_estimation_buffer
+            storage_estimation_buffer = data_storage * estimation_buffer
             data_storage_total = data_storage + storage_buffer + storage_estimation_buffer
         else:
             # data is spread across all VMs
